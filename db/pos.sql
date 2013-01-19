5802431405f2398de759f17113ceb8b882eda39e
unknown <bluestraenje@gmail.com>
2013-01-19 13:32:13 +0800

    fix defaults

--- db/pos.sql
+++ db/pos.sql
@@ -1,12 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.47, for Win32 (ia32)
+-- MySQL dump 10.13  Distrib 5.5.29, for Win32 (x86)
 --
--- Host: localhost    Database: pos
+-- Host: localhost    Database: db_pos
 -- ------------------------------------------------------
--- Server version	5.1.47-community-log
-
-drop database if exists `db_pos`;
-create database db_pos;
-use db_pos;
+-- Server version	5.5.29-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -19,6 +15,10 @@ use db_pos;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+DROP DATABASE IF EXISTS `db_pos`;
+CREATE DATABASE `db_pos`;
+USE `db_pos`;
+
 --
 -- Table structure for table `accesssite`
 --
@@ -27,23 +27,68 @@ DROP TABLE IF EXISTS `accesssite`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `accesssite` (
-  `AccessSiteID` int(11) NOT NULL,
+  `AccessSiteID` int(11) NOT NULL AUTO_INCREMENT,
   `SiteName` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`AccessSiteID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `catergoryinfo`
+-- Table structure for table `categoryinfo`
 --
 
-DROP TABLE IF EXISTS `catergoryinfo`;
+DROP TABLE IF EXISTS `categoryinfo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `catergoryinfo` (
-  `CategoryID` int(11) NOT NULL,
+CREATE TABLE `categoryinfo` (
+  `CategoryID` int(11) NOT NULL AUTO_INCREMENT,
   `CategoryName` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`CategoryID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `genericinfo`
+--
+
+DROP TABLE IF EXISTS `genericinfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `genericinfo` (
+  `genericID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `genericName` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`genericID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `iteminfo`
+--
+
+DROP TABLE IF EXISTS `iteminfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `iteminfo` (
+  `ItemID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `ItemName` varchar(300) DEFAULT NULL,
+  `Description` varchar(750) DEFAULT NULL,
+  `GenericID` bigint(20) NOT NULL,
+  `CategoryID` int(11) NOT NULL,
+  `Manufacturer` varchar(250) DEFAULT NULL,
+  `Price` double NOT NULL DEFAULT '0',
+  `Markup` smallint(6) NOT NULL DEFAULT '0',
+  `OriginalStock` int(11) NOT NULL DEFAULT '0',
+  `QtyStock` int(11) NOT NULL DEFAULT '0',
+  `QtySold` int(11) NOT NULL DEFAULT '0',
+  `ThresholdValue` int(11) NOT NULL DEFAULT '0',
+  `Barcode` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`ItemID`),
+  KEY `FK_iteminfo_1` (`CategoryID`),
+  KEY `FK_iteminfo_2` (`GenericID`),
+  CONSTRAINT `FK_iteminfo_1` FOREIGN KEY (`CategoryID`) REFERENCES `categoryinfo` (`CategoryID`) ON UPDATE CASCADE,
+  CONSTRAINT `FK_iteminfo_2` FOREIGN KEY (`GenericID`) REFERENCES `genericinfo` (`genericID`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -55,54 +100,12 @@ DROP TABLE IF EXISTS `daysummary`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `daysummary` (
-  `DayID` bigint(20) NOT NULL,
+  `DayID` bigint(20) NOT NULL AUTO_INCREMENT,
   `DayStamp` datetime DEFAULT NULL,
-  `TotalItemSold` int(11) DEFAULT NULL,
-  `TotalSales` double DEFAULT NULL,
-  `TotalTax` double DEFAULT NULL,
+  `TotalItemSold` int(11) NOT NULL DEFAULT '0',
+  `TotalSales` double NOT NULL DEFAULT '0',
+  `TotalTax` double NOT NULL DEFAULT '0',
   PRIMARY KEY (`DayID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `genericinfo`
---
-
-DROP TABLE IF EXISTS `genericinfo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `genericinfo` (
-  `genericID` bigint(20) NOT NULL,
-  `genericName` varchar(500) DEFAULT NULL,
-  PRIMARY KEY (`genericID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `iteminfo`
---
-
-DROP TABLE IF EXISTS `iteminfo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `iteminfo` (
-  `ItemID` bigint(20) NOT NULL,
-  `ItemName` varchar(300) DEFAULT NULL,
-  `Description` varchar(750) DEFAULT NULL,
-  `GenericID` bigint(20) DEFAULT NULL,
-  `CategoryID` int(11) DEFAULT NULL,
-  `Manufacturer` varchar(250) DEFAULT NULL,
-  `Price` double DEFAULT NULL,
-  `Markup` smallint(6) DEFAULT NULL,
-  `OriginalStock` int(11) DEFAULT NULL,
-  `QtyStock` int(11) DEFAULT NULL,
-  `QtySold` int(11) DEFAULT NULL,
-  `ThresholdValue` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ItemID`),
-  KEY `FK_iteminfo_1` (`CategoryID`),
-  KEY `FK_iteminfo_2` (`GenericID`),
-  CONSTRAINT `FK_iteminfo_1` FOREIGN KEY (`CategoryID`) REFERENCES `catergoryinfo` (`CategoryID`) ON UPDATE CASCADE,
-  CONSTRAINT `FK_iteminfo_2` FOREIGN KEY (`GenericID`) REFERENCES `genericinfo` (`genericID`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -114,10 +117,10 @@ DROP TABLE IF EXISTS `iteminventory`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `iteminventory` (
-  `InventoryID` bigint(20) NOT NULL,
+  `InventoryID` bigint(20) NOT NULL AUTO_INCREMENT,
   `DayStamp` datetime DEFAULT NULL,
-  `StockAdded` int(11) DEFAULT NULL,
-  `StockRemoved` int(11) DEFAULT NULL,
+  `StockAdded` int(11) NOT NULL DEFAULT '0',
+  `StockRemoved` int(11) NOT NULL DEFAULT '0',
   `ItemID` bigint(20) NOT NULL,
   PRIMARY KEY (`InventoryID`),
   KEY `FK_iteminventory_1` (`ItemID`),
@@ -133,12 +136,13 @@ DROP TABLE IF EXISTS `rolesheader`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `rolesheader` (
-  `RoleID` int(11) NOT NULL,
+  `RoleID` int(11) NOT NULL AUTO_INCREMENT,
   `RoleName` varchar(50) DEFAULT NULL,
   `Description` varchar(300) DEFAULT NULL,
   PRIMARY KEY (`RoleID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `rolesdetail`
@@ -148,7 +152,7 @@ DROP TABLE IF EXISTS `rolesdetail`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `rolesdetail` (
-  `DetailID` int(11) NOT NULL,
+  `DetailID` int(11) NOT NULL AUTO_INCREMENT,
   `AccessSiteID` int(11) NOT NULL,
   `RoleID` int(10) NOT NULL,
   PRIMARY KEY (`DetailID`),
@@ -159,7 +163,6 @@ CREATE TABLE `rolesdetail` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-
 --
 -- Table structure for table `salesorderheader`
 --
@@ -168,18 +171,19 @@ DROP TABLE IF EXISTS `salesorderheader`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `salesorderheader` (
-  `SalesOrderID` bigint(20) NOT NULL,
-  `TotalAmount` double DEFAULT NULL,
-  `Tax` double DEFAULT NULL,
-  `AmountDue` double DEFAULT NULL,
-  `TotalQty` int(11) DEFAULT NULL,
+  `SalesOrderID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `TotalAmount` double NOT NULL DEFAULT '0',
+  `Tax` double NOT NULL DEFAULT '0',
+  `AmountDue` double NOT NULL DEFAULT '0',
+  `TotalQty` int(11) NOT NULL DEFAULT '0',
   `DayStamp` datetime DEFAULT NULL,
-  `DayID` bigint(20) DEFAULT NULL,
+  `DayID` bigint(20) NOT NULL,
   PRIMARY KEY (`SalesOrderID`),
   KEY `FK_salesorderheader_1` (`DayID`),
   CONSTRAINT `FK_salesorderheader_1` FOREIGN KEY (`DayID`) REFERENCES `daysummary` (`DayID`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `salesorderdetail`
@@ -191,12 +195,14 @@ DROP TABLE IF EXISTS `salesorderdetail`;
 CREATE TABLE `salesorderdetail` (
   `DetailID` bigint(20) NOT NULL,
   `SalesOrderID` bigint(20) NOT NULL,
-  `ItemID` bigint(20) DEFAULT NULL,
-  `Qty` int(11) DEFAULT NULL,
-  `Amount` double DEFAULT NULL,
+  `ItemID` bigint(20) NOT NULL,
+  `Qty` int(11) NOT NULL DEFAULT '0',
+  `Amount` double NOT NULL DEFAULT '0',
   PRIMARY KEY (`DetailID`),
   KEY `FK_salesorderdetail_1` (`SalesOrderID`),
-  CONSTRAINT `FK_salesorderdetail_1` FOREIGN KEY (`SalesOrderID`) REFERENCES `salesorderheader` (`SalesOrderID`) ON UPDATE CASCADE
+  KEY `FK_salesorderdetail_2` (`ItemID`),
+  CONSTRAINT `FK_salesorderdetail_1` FOREIGN KEY (`SalesOrderID`) REFERENCES `salesorderheader` (`SalesOrderID`) ON UPDATE CASCADE,
+  CONSTRAINT `FK_salesorderdetail_2` FOREIGN KEY (`ItemID`) REFERENCES `iteminfo` (`ItemID`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -209,25 +215,15 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `UserID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL AUTO_INCREMENT,
   `UserName` varchar(250) DEFAULT NULL,
   `Password` varchar(50) DEFAULT NULL,
-  `RoleID` int(11) DEFAULT NULL,
+  `RoleID` int(11) NOT NULL,
   PRIMARY KEY (`UserID`),
   KEY `FK_user_1` (`RoleID`),
   CONSTRAINT `FK_user_1` FOREIGN KEY (`RoleID`) REFERENCES `rolesheader` (`RoleID`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
 
 --
 -- Table structure for table `payment`
@@ -237,13 +233,13 @@ DROP TABLE IF EXISTS `payment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `payment` (
-  `ORID` bigint(20) NOT NULL,
-  `SalesOrderID` bigint(20) DEFAULT NULL,
-  `TotalAmount` double DEFAULT NULL,
-  `Tax` double DEFAULT NULL,
-  `AmountDue` double DEFAULT NULL,
-  `Change` double DEFAULT NULL,
-  `UserID` int(11) DEFAULT NULL,
+  `ORID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `SalesOrderID` bigint(20) NOT NULL,
+  `TotalAmount` double NOT NULL DEFAULT '0',
+  `Tax` double NOT NULL DEFAULT '0',
+  `AmountDue` double NOT NULL DEFAULT '0',
+  `Change` double NOT NULL DEFAULT '0',
+  `UserID` int(11) NOT NULL,
   `DayStamp` datetime DEFAULT NULL,
   PRIMARY KEY (`ORID`),
   KEY `FK_payment_1` (`SalesOrderID`),
@@ -254,4 +250,14 @@ CREATE TABLE `payment` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
--- Dump completed on 2013-01-17 21:25:52
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-01-19 13:22:04
+
