f42cb8b62ad3f18905f250b414b1f64d0860a00c
marvin <marvin.casagnap@gmail.com>
2013-02-06 21:16:43 +0800

    commit changes...

--- src/NJournals/NJournals.Core/Views/LaundryView.cs
+++ src/NJournals/NJournals.Core/Views/LaundryView.cs
@@ -126,12 +126,15 @@ namespace NJournals.Core.Views
 			LaundryPriceSchemeDao priceDao = new LaundryPriceSchemeDao();
 			LaundryPriceSchemeDataEntity priceEntity = new LaundryPriceSchemeDataEntity();
 			priceEntity = priceDao.GetByCategoryService(cmbcategory.Text, cmbservices.Text);
-			
+			double kilo = 0;
+			double.TryParse(txtkilo.Text, out kilo);
+			double price = priceEntity.Price * kilo;			
 			IList<String> lstItems = new List<String>();
 			lstItems.Add(cmbcategory.Text);
 			lstItems.Add(cmbservices.Text);
 			lstItems.Add(txtnoitems.Text);
 			lstItems.Add(txtkilo.Text);
+			lstItems.Add(price.ToString());
 			string[] items = new string[lstItems.Count];
 			lstItems.CopyTo(items,0);
 			dataGridView1.Rows.Add(items);
