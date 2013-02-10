f1d533f2bda8b11649b1e4abe153745e98dad252
marvinc <marvin.casagnap@gmail.com>
2013-02-10 23:23:12 +0800

    commit changes

---
+++ src/NJournals/NJournals.Common/Interfaces/ICheckListView.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: matt
+ * Date: 2/10/2013
+ * Time: 9:50 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NJournals.Common.DataEntities;
+using System.Collections.Generic;
+
+namespace NJournals.Common.Interfaces
+{
+	/// <summary>
+	/// Description of ICheckListView.
+	/// </summary>
+	public interface ICheckListView : IView
+	{
+		void SetAllCheckList(List<LaundryChecklistDataEntity> checkListEntities);
+	}
+}
