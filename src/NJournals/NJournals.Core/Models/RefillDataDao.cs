337f589e539405ec230d4b98c615107d36e50ad2
marvin <marvin.casagnap@gmail.com>
2013-02-18 23:01:32 +0800

    add refilldatadao

---
+++ src/NJournals/NJournals.Core/Models/RefillDataDao.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mc185104
+ * Date: 2/18/2013
+ * Time: 6:17 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace NJournals.Core.Models
+{
+	/// <summary>
+	/// Description of RefillDataDao.
+	/// </summary>
+	public class RefillDataDao
+	{
+		public RefillDataDao()
+		{
+		}
+	}
+}
--- src/NJournals/NJournals.Core/Views/RefillingView.Designer.cs
+++ src/NJournals/NJournals.Core/Views/RefillingView.Designer.cs
@@ -259,6 +259,8 @@ namespace NJournals.Core.Views
 			// 
 			// dataGridView1
 			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
 			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
 									this.Column1,
